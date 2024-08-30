fe9508373a994f1a0dbf5f671bb2eb32f9be3bff
Karan Kuthe <karankuthe132@gmail.com>
2024-08-31 01:09:35 +0530

    deployed the contract

--- frontend/src/Wallet/Wallet.tsx
+++ frontend/src/Wallet/Wallet.tsx
@@ -2,28 +2,35 @@ import styles from './wallet.module.css'
 import {useState} from "react";
 import Auth from '../WebAuth/Auth.tsx'
 import {Stake} from "../Supply/Stake.tsx";
+import {Deploy} from "../DeployProvider/DeployProvider.tsx";
+
 const Wallet = () =>{
 const [visible , setVisible] = useState(true)
     return(
         <>
             <div className={styles.backgroundContainer}>
-                 <div className={styles.connectDiv} >
-                     <div className={styles.headingStyle}>
-                         {visible ?   <h1>Connect Your Wallet to Get Started</h1> : null}
-                     </div>
-                     <div className={styles.authContainer}>
-                         <Auth visible={visible} setVisible = {setVisible}/>
-                     </div>
-                 </div>
-                 <div className={styles.supplyDiv}>
+                <div className={styles.randomDiv}>
+                    <div className={styles.connectDiv}>
+                        <div className={styles.headingStyle}>
+                            {visible ? <h1>Connect Your Wallet to Get Started</h1> : null}
+                        </div>
+                        <div className={styles.authContainer}>
+                            <Auth visible={visible} setVisible={setVisible} />
+                        </div>
+                    </div>
+                    <div className={styles.supplyDiv}>
                         <div className={styles.headingStyle}>
                             <h1>Supply Randomness</h1>
                         </div>
-                       <div>
-                           <Stake />
-                       </div>
-                 </div>
+                        <div>
+                            <Stake/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                </div>
             </div>
+
         </>
     )
 }
