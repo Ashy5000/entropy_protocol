2353f67ae1b483ace42e249f8cb14f7817785349
Ashy5000 <ashydashy5000@gmail.com>
2024-09-07 12:10:56 -0700

    Adds bits commited tracker

--- frontend/src/Hero/Hero.tsx
+++ frontend/src/Hero/Hero.tsx
@@ -1,20 +1,42 @@
-import styles from './hero.module.css'
-import { useScramble } from "use-scramble"
+import styles from "./hero.module.css";
+import { useState } from "react";
+import { useScramble } from "use-scramble";
+import { IndexService } from "@ethsign/sp-sdk";
 
 const Hero = () => {
-    const { ref } = useScramble({
-        text: "Randomness Redefined",
-        speed:0.35
-    });
+  const [bitsCommited, setBitsCommited] = useState(0);
+  const { ref } = useScramble({
+    text: "Randomness Redefined",
+    speed: 0.35,
+  });
 
-    return(
-        <>
-           <div className={styles.heroContainer}>
-               <h1 className={styles.heroText} ref={ref}></h1>
+  const indexService = new IndexService("testnet");
+  const attestationsPromise = indexService.queryAttestationList({
+    schemaId: "onchain_evm_11155111_0x79",
+    page: 1,
+  });
+  attestationsPromise.then(function (attestations) {
+    if (attestations != null) {
+      console.log(attestations);
+      setBitsCommited(attestations.total * 256);
+    }
+  });
 
-           </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.background}>
+        <div className={styles.heroContainer}>
+          <h1 className={styles.heroHeading} ref={ref}></h1>
+        </div>
+        <div className={styles.heroContainer}>
+          <p className={styles.heroText}>
+            <span className={styles.number}>{bitsCommited}</span> bits of
+            randomness commited to date
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
